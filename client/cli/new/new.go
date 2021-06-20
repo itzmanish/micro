b1bdc57c28514d51ed2440f937300ec332df22ca
Manish <itzmanish108@gmail.com>
2021-06-20 15:00:35 +0530

    fix path of protoc-gen-micro

--- client/cli/new/new.go
+++ client/cli/new/new.go
@@ -27,7 +27,7 @@ func protoComments(goDir, alias string) []string {
 		"\ndownload protobuf for micro:\n",
 		"go get -u github.com/golang/protobuf/proto",
 		"go get -u github.com/golang/protobuf/protoc-gen-go",
-		"go get github.com/itzmanish/micro/v2/cmd/protoc-gen-micro",
+		"go get github.com/itzmanish/protoc-gen-micro",
 		"\ncompile the proto file " + alias + ".proto:\n",
 		"cd " + goDir,
 		"make proto\n",
